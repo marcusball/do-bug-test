c2edee29ab1fb589aac82e9c698c942cef4a24de
Marcus Ball <marcus.ball@live.com>
2022-02-23 23:57:57 -0500

    TEAR DOWN THAT BUFFERING

--- nginx.conf.php
+++ nginx.conf.php
@@ -18,7 +18,8 @@ http {
 
         server_tokens off;
 
-        fastcgi_buffers 8 4k;
+        fastcgi_buffering off;
+        # fastcgi_buffers 8 4k;
 
         # define an easy to reference name that can be used in fastgi_pass
         upstream heroku-fcgi {
